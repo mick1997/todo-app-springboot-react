66781bc4aca832b4cabc8d958735d8a7956fa892
JackFengFile <mick1997@users.noreply.github.com>
2023-03-27 11:39:05 -0400

    Update JwtTokenRequest.java

--- restful-web-services/src/main/java/com/todoapp/rest/webservices/restfulwebservices/jwt/resource/JwtTokenRequest.java
+++ restful-web-services/src/main/java/com/todoapp/rest/webservices/restfulwebservices/jwt/resource/JwtTokenRequest.java
@@ -6,9 +6,6 @@ public class  JwtTokenRequest implements Serializable {
 
   private static final long serialVersionUID = -5616176897013108345L;
 
-//    {
-//        "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJpbjI4bWludXRlcyIsImV4cCI6MTU5MDE0MDUzNCwiaWF0IjoxNTg5NTM1NzM0fQ.TRGChbMBWcf6xa7oUge7Ps6pHf_X3vmCf6z06ZTxyRujWObXXRjzZhAhYmocBIaiyzRWgFC9uMv2HZT74kKQqw"
-//    }
   private String username;
     private String password;
 
